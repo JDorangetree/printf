a1540f4d322c3bff2938168278646e36719c2d97
Campo Pinillos <campopinillos@MacBook-Pro-de-Campo.local>
2019-10-28 18:07:21 -0500

    [Fix] Binary 0 case

--- print_bynary.c
+++ print_bynary.c
@@ -11,30 +11,35 @@
 char *print_binary(int n)
 {
 	int c, d, count, flag = 0;
-	char *pointer;
+	char *pointer, *cero = "0";
 
 	count = 0;
-	pointer = (char *) malloc(33);
-	if (pointer == NULL)
-		exit(EXIT_FAILURE);
-	for (c = 31 ; c >= 0 ; c--)
+	if (n == 0)
+		return (cero);
+	else
 	{
-		d = n >> c;
-		if (d & 1)
-			*(pointer + count) = 1 + '0';
-		else
-			*(pointer + count) = 0 + '0';
-		count++;
-	}
-	*(pointer + count) = '\0';
-	while (pointer)
-	{
-		if (*pointer != '0')
-			flag = 1;
-		if (flag == 1)
-			return (pointer);
-		pointer++;
+		pointer = (char *) malloc(33);
+		if (!pointer)
+			exit(EXIT_FAILURE);
+		for (c = 31 ; c >= 0 ; c--)
+		{
+			d = n >> c;
+			if (d & 1)
+				*(pointer + count) = 1 + '0';
+			else
+				*(pointer + count) = 0 + '0';
+			count++;
+		}
+		*(pointer + count) = '\0';
+		while (pointer)
+		{
+			if (*pointer != '0')
+				flag = 1;
+			if (flag == 1)
+				return (pointer);
+			pointer++;
+		}
 	}
 	free(pointer);
-	return (NULL);
+	return (pointer);
 }
