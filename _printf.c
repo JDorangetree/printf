899b375680ae45d4132a23e58eadc0af28ed2bf3
Campo Elías Pinillos Galindo <46058964+campopinillos@users.noreply.github.com>
2019-10-28 12:40:55 -0500

    [Upload] Files to check (#1)
    * [Feature] Upload files
    
    * [Feature] Backup files
    
    * [Update] New format
    
    * Delete copy.md
    
    * [Update] file
    
    * [Delete] Unnecesary file
    
    * [Delete] special_char.c remove
    
    * [Update] Files in my branch
    
    * [Fix] Betty style requirements
    
    * [Fix] Betty style p_char p_int files
    
    * [fix] Betty style
    
    * [fix] betty style
    
    * [Fix] function

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * _printf - Print function all the parameters
+ * @format: Input parameters
+ *
+ * Description: Print the inputs passed to the
+ * function.
+ * Return: The number of printed characters
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, i_b = 0;
+	char *ptr = (char *) format, *buffer;
+	int (*ptr_f)(va_list, char *, int);
+	va_list arguments;
+
+	va_start(arguments, format);
+	buffer = malloc(sizeof(char) * SIZE);
+	if (!buffer)
+		return (1);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			buffer[i_b] = format[i];
+			i_b++;
+		}
+		else
+		{
+			ptr_f = get_type(++ptr);
+			i_b = ptr_f(arguments, buffer, i_b);
+			i++;
+		}
+		i++, ptr++;
+	}
+	va_end(arguments);
+	write(1, buffer, i_b);
+	free(buffer);
+	return (i_b);
+}
---
+++ holberton.h
@@ -0,0 +1,34 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#define SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+
+int _printf(const char *format, ...);
+int (*get_type(char *s))(va_list arguments, char *buffer, int i_b);
+int p_char(va_list arguments, char *buffer, int i_b);
+int p_string(va_list arguments, char *buffer, int i_b);
+int p_int(va_list arguments, char *buffer, int i_b);
+int percent(va_list arguments, char *buffer, int i_b);
+int print_number(int n, char *buffer, int i_b);
+int _putchar(char c);
+
+/**
+ * struct t_input - Struct t_input
+ * @letter: The class
+ * @f: The function associated
+ *
+ * Description: This is a longer description
+ */
+typedef struct t_input
+{
+	char *letter;
+	int (*f)(va_list arguments, char *buffer, int i_b);
+} t_i;
+
+#endif
