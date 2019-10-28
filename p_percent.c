7bb57d18a41b0056a0030dc6fa9cff3bca9b7d04
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-28 17:30:54 +0000

    [fix] betty style

--- p_percent.c
+++ p_percent.c
@@ -1,7 +1,15 @@
 #include "holberton.h"
-
+/**
+ * percent - Print %
+ * @arguments: list of arguments passed to printf
+ * @buffer: memory field to host the printf output
+ * @i_b: buffer position
+ *
+ * Description: print %
+ * Return: 0
+ */
 int percent(va_list arguments, char *buffer, int i_b)
 {
 	buffer[i_b] = '%';
-	return(++i_b);
+	return (++i_b);
 }
--- p_string.c
+++ p_string.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
-
+/**
+ * p_string - Print string
+ * @arguments: input string
+ * @buffer: memory field to host the printf output
+ * @i_b: buffer position
+ *
+ * Description: print number
+ * Return: 0
+ */
 int p_string(va_list arguments, char *buffer, int i_b)
 {
 	int i = 0;
@@ -7,5 +15,5 @@ int p_string(va_list arguments, char *buffer, int i_b)
 
 	for (; ptr[i]; i++, i_b++)
 		buffer[i_b] = ptr[i];
-	return(i_b);
+	return (i_b);
 }
