babfcd9a939eb3bf753749b9869b2a3e88134b14
Campo Pinillos <campopinillos@MacBook-Pro-de-Campo.local>
2019-10-29 10:55:22 -0500

    [Feature] Files to run rot13 and reverse print

--- print_bynary.c
+++ print_bynary.c
@@ -37,10 +37,6 @@ char *print_binary(int n)
 			return (pointer);
 		pointer++;
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 83e81ce91617e76a48b48dabc67692793e2ea628
 	free(pointer);
 	return (pointer);
 }
