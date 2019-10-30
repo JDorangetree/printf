cab8a4808c93f69fd3f690a8cde4564f0f2164da
Campo Pinillos <campopinillos@MacBook-Pro-de-Campo.local>
2019-10-30 09:16:06 -0500

    [Fix] Reverse

--- p_reverse.c
+++ p_reverse.c
@@ -1,34 +1,15 @@
 #include "holberton.h"
 /**
-<<<<<<< HEAD
- * p_reverse - Print string reverse
-=======
  * p_string - Print string
->>>>>>> 732b6ac49596a3d43809648b69c1974f8a68f89a
  * @arguments: input string
  * @buffer: memory field to host the printf output
  * @i_b: buffer position
  *
-<<<<<<< HEAD
- * Description: print string reverse
-=======
  * Description: print number
->>>>>>> 732b6ac49596a3d43809648b69c1974f8a68f89a
  * Return: 0
  */
 int p_reverse(va_list arguments, char *buffer, int i_b)
 {
-<<<<<<< HEAD
-	int n = 0;
-	char *ptr = va_arg(arguments, char *);
-
-	if (!ptr)
-		ptr = ")llun(";
-	n = _strlen(ptr) - 1;
-	while (n >= 0)
-	{
-		buffer[i_b] = ptr[n];
-=======
 	int i = 0;
 	int n = 0;
 	char pointer;
@@ -44,7 +25,6 @@ int p_reverse(va_list arguments, char *buffer, int i_b)
 	while (n >= 0)
 	{
 		buffer[i_b] = pointer[n];
->>>>>>> 732b6ac49596a3d43809648b69c1974f8a68f89a
 		n--;
 		i_b++;
 	}
